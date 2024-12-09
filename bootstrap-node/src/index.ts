72f905946b6456e6c84649ef404d62f79dcbb55f
Nadeesha Cabral <n@nadeesha.me>
2024-12-09 13:54:20 +1100

    fix: Simplify the quickstart

--- bootstrap-node/src/index.ts
+++ bootstrap-node/src/index.ts
@@ -1,48 +1,35 @@
 import { Inferable } from "inferable";
 import { z } from "zod";
+import { execFile } from "child_process";
+import { promisify } from "util";
 
-// Some mock functions to register
-import * as functions from "./functions";
+const execFilePromise = promisify(execFile);
 
-// Instantiate the Inferable client.
 const client = new Inferable({
-  // To get a new key, run:
-  // npx @inferable/cli auth keys create 'My New Machine Key' --type='cluster_machine'
+  // Get your key from https://app.inferable.ai/clusters
   apiSecret: process.env.INFERABLE_API_SECRET,
 });
 
-// Register some demo functions
 client.default.register({
-  name: "getUrlContent",
-  func: functions.getUrlContent,
-  description: "Gets the content of a URL",
-  schema: {
-    input: z.object({
-      url: z.string().describe("The URL to get the content of"),
-    }),
+  name: "exec",
+  func: async ({ command, arg }: { command: string; arg?: string }) => {
+    const args = arg ? [arg] : [];
+    const { stdout, stderr } = await execFilePromise(command, args);
+    return {
+      stdout: stdout.trim(),
+      stderr: stderr.trim(),
+    };
   },
-});
-
-client.default.register({
-  name: "generatePage",
-  func: functions.generatePage,
-  description: "Generates a page from markdown",
+  description: "Executes a system command",
   schema: {
     input: z.object({
-      markdown: z.string().describe("The markdown to generate a page from"),
-    }),
-  },
-});
-
-client.default.register({
-  name: "scoreHNPost",
-  func: functions.scoreHNPost,
-  description:
-    "Calculates a score for a Hacker News post given its comment count and upvotes",
-  schema: {
-    input: z.object({
-      commentCount: z.number().describe("The number of comments"),
-      upvotes: z.number().describe("The number of upvotes"),
+      command: z
+        .enum(["pwd", "ls", "cat", "echo"]) // This prevents arbitrary commands
+        .describe("The command to execute"),
+      arg: z
+        .string()
+        .describe("The argument to pass to the command")
+        .optional(),
     }),
   },
 });
@@ -50,5 +37,3 @@ client.default.register({
 client.default.start().then(() => {
   console.log("Inferable demo service started");
 });
-
-// To trigger a run: tsx -r dotenv/config src/run.ts
