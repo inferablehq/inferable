d01fcaeafaedebeac7f6215fc83cf443589e9af1
John Smith <john@johnjcsmith.com>
2024-12-16 19:44:45 +1030

    feat: Support pointer references

--- sdk-go/service.go
+++ sdk-go/service.go
@@ -105,13 +105,19 @@ func (s *service) RegisterFunc(fn Function) (*FunctionReference, error) {
 		return nil, fmt.Errorf("function '%s' must have exactly one argument", fn.Name)
 	}
 	argType := fnType.In(0)
-	if argType.Kind() != reflect.Struct {
-		return nil, fmt.Errorf("function '%s' first argument must be a struct", fn.Name)
-	}
 
-	// Get the schema for the input struct
-	reflector := jsonschema.Reflector{}
-	schema := reflector.Reflect(reflect.New(argType).Interface())
+  // Set the argument type to the referenced type
+  if argType.Kind() == reflect.Ptr {
+    argType = argType.Elem()
+  }
+
+  if argType.Kind() != reflect.Struct {
+    return nil, fmt.Errorf("function '%s' first argument must be a struct or a pointer to a struct", fn.Name)
+  }
+
+  // Get the schema for the input struct
+  reflector := jsonschema.Reflector{DoNotReference: true}
+  schema := reflector.Reflect(reflect.New(argType).Interface())
 
 	if schema == nil {
 		return nil, fmt.Errorf("failed to get schema for function '%s'", fn.Name)
