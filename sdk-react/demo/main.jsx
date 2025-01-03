453280d9018c55b363cd1b8e303db7654eb8c184
Nadeesha Cabral <n@nadeesha.me>
2025-01-03 18:55:04 +1100

    chore: Update README and demo for improved SDK usage examples

--- sdk-react/demo/main.jsx
+++ sdk-react/demo/main.jsx
@@ -4,10 +4,10 @@ import { TestPage } from "./TestPage";
 
 const insecureClusterAuth = {
   authType: "cluster",
-  clusterId: "01JGFZ1E1HJGBA4893P5WE7P6Q",
-  apiSecret: "sk_yyGmpQtEkVv5WOrrZ9ukvjr0Ub1lj2anrC6bhksHVw",
+  clusterId: "",
+  apiSecret: "",
 };
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <TestPage baseUrl="http://localhost:4000" {...insecureClusterAuth} />
+  <TestPage baseUrl="https://api.inferable.ai" {...insecureClusterAuth} />
 );
